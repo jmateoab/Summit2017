c3912a6b594c9f8bedce78d4abbbfcd20b7ad2ba
José Antonio Mateo Cortés <jmateo@solidq.com>
2017-05-21 09:55:48 +0200

    Initial Commit

---
+++ StartStopVM.ps1
@@ -0,0 +1,8 @@
+﻿workflow StartStopVM
+{
+   $Conn = Get-AutomationConnection -Name AzureRunAsConnection
+    Add-AzureRMAccount -ServicePrincipal -Tenant $Conn.TenantID `
+    -ApplicationId $Conn.ApplicationID -CertificateThumbprint $Conn.CertificateThumbprint
+   # Start-AzureRmVM -Name 'Summit2017-VM-ONOFF' -ResourceGroupName 'summit2017_rg'
+   Stop-AzureRmVM -Name 'Summit2017-VM-ONOFF' -ResourceGroupName 'summit2017_rg'
+}
